f8a6be6b9fd6bcdd4a0fa2b68219431cd406473d
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2023-09-14 23:13:02 +0000

    :rose: test

--- src/api/models.py
+++ src/api/models.py
@@ -9,6 +9,11 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.is_active = True
+ 
     def __repr__(self):
         return f'<User {self.email}>'
 
@@ -16,7 +21,6 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
     
 
@@ -28,6 +32,18 @@ class Address(db.Model):
     address = db.Column(db.String(80), nullable=False)
     postal_code = db.Column(db.String(40), nullable=False)
 
+    def __init__(self, country, city, address, postal_code):
+        self.country = country
+        self.city = city
+        self.address = address
+        self.postal_code = postal_code
+
+    def serialize(self):
+        return {
+            "city" : self.city,
+            "address" : self.address
+        }
+
 
 class Customer(User):
 
@@ -36,6 +52,18 @@ class Customer(User):
     #user = db.relationship("User")
     delivery_address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
     delivery_address = db.relationship('Address')
+
+    def __init__(self, email, password, address):
+        super().__init__(email=email, password=password)
+        self.address = address
+
+    def serialize(self):
+        return {
+            "email" : self.email,
+            "matricula": self.matricula,
+            "address": self.address.serialize()
+        }
+
 
 
 class VehicleType(enum.Enum):
@@ -48,11 +76,50 @@ class Driver(User):
     matricula = db.Column(db.String(10), nullable=False)
     vehicle = db.Column(db.Enum(VehicleType), nullable=False)
 
-association_table = db.Table(
-    "association_table_orders",
-    db.Column("products", db.Integer ,db.ForeignKey("products.id")),
-    db.Column("ordenes", db.Integer ,db.ForeignKey("ordenes.id")),
-)
+    def __init__(self, email, password, matricula, vehicle):
+        super().__init__(email=email, password=password)
+        self.matricula = matricula
+        self.vehicle = vehicle
+
+    def serialize(self):
+        return {
+            "email" : self.email,
+            "matricula": self.matricula,
+            "vehicle": self.vehicle.value
+        }
+        
+    
+
+# association_table = db.Table(
+#     "association_table_orders",
+#     db.Column("products", db.Integer ,db.ForeignKey("products.id")),
+#     db.Column("ordenes", db.Integer ,db.ForeignKey("ordenes.id")),
+# )
+
+class Order(db.Model):
+
+    __tablename__ = "ordenes"
+    id = db.Column(db.Integer, primary_key=True)
+
+    # delivery_id = db.Column(db.Integer, db.ForeignKey('driver.id'))
+    # delivery = db.relationship("Driver")
+
+    # customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
+    # customer = db.relationship('Customer')
+
+    #items = db.relationship("Products", secondary=association_table, backref="active_orders")
+
+    def __init__(self, customer, delivery, items):
+        # self.customer = customer
+        # self.delivery = delivery
+        self.items = items
+
+    def serialize(self):
+        return {
+            "delivery": self.delivery.serialize(),
+            #"products" : [ pro.serialize() for pro in self.products]
+        }
+
 class Products(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
@@ -61,16 +128,31 @@ class Products(db.Model):
     amount = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float(4,2), nullable=False)
 
+    def __init__(self, name, description, amount, price):
+        self.name = name
+        self.description = description
+        self.amount = amount
+        self.price = price
 
-class Order(db.Model):
+    @classmethod
+    def new_product(cls, name, description, amount, price):
+        new_prod = cls(name, description, amount, price)
+        db.session.add(new_prod)
+        try:
+            db.session.commit()
+            return new_prod
+        except Exception as error:
+            print(error)
+            return None
 
-    __tablename__ = "ordenes"
-    id = db.Column(db.Integer, primary_key=True)
+    def serialize(self):
+        return {
+            "name" : self.name,
+            "price" : self.price
+        }
 
-    delivery_id = db.Column(db.Integer, db.ForeignKey('driver.id'))
-    custumer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
 
-    products = db.relationship("Products", secondary=association_table)
+
 
 
 
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Driver, VehicleType, Products, Order, Customer, Address
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,43 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/account/<string:tipo>', methods=['POST'])
+def create_account(tipo):
+
+    if tipo == 'driver':
+        
+        # name = request.json.get("name", None)
+
+        new_driver = Driver(email="emaisl@gmail.com", password="paranguacutirimicuaro", matricula="VF346S9", vehicle=VehicleType.MOTO)
+
+        new_address = Address(country="Venezuela", city="Caracas", address="Los Guayabitos", postal_code=1080)
+
+        new_customer = Customer(email="emaisl2@gmasil.com", password="paranguacutirimicuaro2", address=new_address)
+        
+        db.session.add(new_driver)
+        db.session.add(new_address)
+        db.session.add(new_customer)
+        
+        try:
+            
+            arepas = Products.new_product("arepa", "la mejor comida", 1, 1.5)
+            malta = Products.new_product("malta", "la mejor bebida", 1, 1)
+            cachapa = Products.new_product("cachapa", "el mejor desayuno", 1, 3)
+
+            db.session.add(cachapa)
+            db.session.add(malta)
+            db.session.add(arepas)
+
+            new_order = Order(customer=new_customer, delivery=new_driver, products=[arepas, malta, cachapa] )
+
+            db.session.add(new_order)
+            db.session.commit()
+
+            return jsonify(new_order.serialize()), 200
+        except ValueError as err:
+            return "Error "+err, 500
+
+    else:
+        return jsonify({ "message": "Driver created succesfully" }), 400
+
