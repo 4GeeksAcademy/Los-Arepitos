e815a571260dda7f08a8ee6104d5fba18511fa40
diegoguillen70 <137721556+diegoguillen70@users.noreply.github.com>
2023-09-21 19:06:11 +0000

    Generating some products

--- src/front/js/pages/order.js
+++ src/front/js/pages/order.js
@@ -2,14 +2,17 @@ import React from "react";
 import "../../styles/order.css";
 import { useContext } from "react";
 import ProductCard from "../component/productCard";
+import { useSearchParams } from "react-router-dom";
 
 const Order = () => {
+  const products = [{ 'product': 'arepa', 'quantity': 0 }, { 'product': 'cachapa', 'quantity': 0 }, { 'product': 'tequeno', 'quantity': 0 }, { 'product': 'empanada', 'quantity': 0 }, { 'product': 'patacon', 'quantity': 0 }, { 'product': 'pabellon', 'quantity': 0 }, { 'product': 'quesillo', 'quantity': 0 }, { 'product': 'queso', 'quantity': 0 }, { 'product': 'guasacaca', 'quantity': 0 }, { 'product': 'guayoyo', 'quantity': 0 }, { 'product': 'chicha', 'quantity': 0 }, { 'product': 'malta', }];
   return (
-    <div className="container d-flex col-md-8 mt-5">      
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+    <div className="container d-flex flex-row col-md-12 mt-5 flex-wrap overflow-auto">
+      {products.map(({ product, quantity }, item) => {
+        return (
+          <ProductCard key={item} onProduct={product} onQuantity={quantity} />
+        )
+      })}
     </div>
   );
 };
