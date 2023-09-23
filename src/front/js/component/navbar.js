df77f4b8b1820766b926a870403687460e0a5a9b
Roberto J. Vargas D <rvargas@rv-solutions.net>
2023-09-23 02:16:04 +0000

    :zap:Add customer profile, adding name in navbar, add endpoint customer profile

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
 
 export const Navbar = () => {
+	const { store } = useContext(Context)
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">React Boilerplate {store.profile ? store.profile.user.name : ""}</span>
 				</Link>
 				<div className="ml-auto">
-				<Link to="/login">
+					<Link to="/login">
 						<button className="btn btn-primary mx-3"><i className="fa-solid fa-user"></i></button>
 					</Link>
 					<Link to="/order">
