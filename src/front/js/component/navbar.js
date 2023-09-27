bc4c9a252b96179e894acbeffb0a60e95e5c58c8
Roberto J. Vargas D <rvargas@rv-solutions.net>
2023-09-27 18:35:06 +0000

    ⚡ Cambios esteticos al Navbar y logo temporal de Los Arepitos

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,15 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
+import logo from "../../img/logo1.png";
 
 export const Navbar = () => {
-	const { store } = useContext(Context)
+	const { store } = useContext(Context);
+	const [title, setTitle] = useState("");
+	const titulo = ["Los Arepitos", "Comida Venezolana"];
+
+	useEffect(() => {
+		let i = 0;
+		let j = 0;
+		const interval = setInterval(() => {
+			if (j === titulo[i].length) {
+				i = (i + 1) % titulo.length;
+				j = 0;
+			}
+			setTitle(titulo[i].substring(0, j + 1));
+			j++;
+		}, 200);
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-expand-lg navbar-light bg-secondary sticky-top">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate {store.profile ? store.profile?.user?.name : ""}</span>
+					<div className="d-flex align-items-center">
+						<img src={logo} alt="Arepitos Logo" className="me-3" style={{ height: "50px", width: "50px", objectFit: "contain", backgroundColor: "transparent" }} />
+						<span className="navbar-brand text-white text-bold font-weight-bold mr-auto my-0 my-lg-0 ms-2">{title} {store.profile ? store.profile?.user?.name : ""}</span>
+					</div>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/login">
