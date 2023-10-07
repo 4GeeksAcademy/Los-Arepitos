0b69ce14e527dcf95c1d9bd342efc568e8e30339
diegoguillen70 <diegoguillen70@gmail.com>
2023-10-07 20:31:00 +0000

    table style

--- src/front/js/pages/adminProducts.js
+++ src/front/js/pages/adminProducts.js
@@ -2,37 +2,43 @@ import React from "react";
 
 const AdminProducts = () => {
 
-    return <>
-        <table className="table table-dark table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
-        </table>
-    </>
+    return (
+        <section className="d-flex mt-5 mx-5 ">
+            <table className="table table-hover">
+                <thead className="table-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Product</th>
+                        <th scope="col">Descrption</th>
+                        <th scope="col">State</th>
+                        <th scope="col">Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td><div className="d-flex align-items-center">
+                            <img
+                                src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                                alt=""
+                                style={{ width: "45px", height: "45px" }}
+                                className="rounded-circle"
+                            />
+                            <div className="ms-3">
+                                <p className="fw-bold mb-1">Tittle</p>
+
+                            </div>
+                        </div></td>
+                        <td><p className="text-muted mb-0">description</p></td>
+                        <td><span className="badge rounded-pill bg-success">Active</span></td>
+                        <td><span>Quantity</span></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </section>
+    )
+
 }
 
 export default AdminProducts;
