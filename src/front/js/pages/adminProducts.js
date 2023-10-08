c6d0ae765dfac64b99965dad3afce9eb4880ef21
diegoguillen70 <diegoguillen70@gmail.com>
2023-10-08 20:05:32 +0000

    pagination navigate

--- src/front/js/pages/adminProducts.js
+++ src/front/js/pages/adminProducts.js
@@ -2,15 +2,29 @@ import React, { useEffect, useState, useMemo } from "react";
 
 const AdminProducts = () => {
     const [products, setProducts] = useState(null)
+    const [pageView, setPageView] = useState({ view: [1, 2, 3], pages: null })
     useEffect(() => {
         const getProduct = async () => {
             const respond = await fetch('http://localhost:3001/api/products')
             const dataJson = await respond.json()
-            //console.log(dataJson.results)
-            setProducts(dataJson.results)
+            const views = Math.ceil(dataJson.total_pages / 3)
+            setProducts(dataJson)
+            setPageView({ ...pageView, pages: views })
         }
         getProduct()
     }, [])
+
+    function handleClickNext() {
+
+
+        const array = Array.from(pageView.view, (x) => x + 3);
+        setPageView({ view: array })
+    }
+
+    function handleClickPrevious() {
+        const array = Array.from(pageView.view, (x) => x - 3);
+        setPageView({ view: array })
+    }
 
     return (
         <section className="d-flex flex-column mt-5 w-75 mx-auto">
@@ -27,7 +41,7 @@ const AdminProducts = () => {
                 </thead>
                 <tbody>
                     {products &&
-                        products.map((product, item) => {
+                        products.results.map((product, item) => {
                             return (
                                 <tr key={item}>
                                     <th scope="row">{item + 1}</th>
@@ -56,14 +70,22 @@ const AdminProducts = () => {
             </table>
             <nav aria-label="Page navigation example">
                 <ul className="pagination justify-content-center">
-                    <li className="page-item mx-0 disabled">
-                        <a className="page-link" href="#" tabIndex="-1" aria-disabled="true">Previous</a>
-                    </li>
-                    <li className="page-item mx-0"><a className="page-link" href="#">1</a></li>
-                    <li className="page-item mx-0"><a className="page-link" href="#">2</a></li>
-                    <li className="page-item mx-0"><a className="page-link" href="#">3</a></li>
                     <li className="page-item mx-0">
-                        <a className="page-link" href="#">Next</a>
+                        <a className="page-link" tabIndex="-1" aria-disabled="true" onClick={handleClickPrevious}>Previous</a>
+                    </li>
+                    {
+                        pageView.view.map((page, item) => {
+                            console.log(page, products?.total_pages)
+                            if (page <= 0) {
+                                setPageView({ view: [1, 2, 3] })
+                                return
+                            }
+                            return <li key={item} className="page-item mx-0"><a className="page-link" href="#">{page}</a></li>
+                        })
+                    }
+
+                    <li className="page-item mx-0">
+                        <a className="page-link" onClick={handleClickNext}>Next</a>
                     </li>
                 </ul>
             </nav>
