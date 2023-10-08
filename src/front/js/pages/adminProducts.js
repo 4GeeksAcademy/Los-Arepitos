e949ff5fe65e45c292f229b721aa4d08fb74c8e7
diegoguillen70 <diegoguillen70@gmail.com>
2023-10-08 01:51:05 +0000

    Produts added to table

--- src/front/js/pages/adminProducts.js
+++ src/front/js/pages/adminProducts.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState, useMemo } from "react";
 
 const AdminProducts = () => {
+    const [products, setProducts] = useState(null)
+    useEffect(() => {
+        const getProduct = async () => {
+            const respond = await fetch('http://localhost:3001/api/products')
+            const dataJson = await respond.json()
+            //console.log(dataJson.results)
+            setProducts(dataJson.results)
+        }
+        getProduct()
+    }, [])
 
     return (
         <section className="d-flex mt-5 mx-5 ">
@@ -15,24 +25,30 @@ const AdminProducts = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td><div className="d-flex align-items-center">
-                            <img
-                                src="https://mdbootstrap.com/img/new/avatars/8.jpg"
-                                alt=""
-                                style={{ width: "45px", height: "45px" }}
-                                className="rounded-circle"
-                            />
-                            <div className="ms-3">
-                                <p className="fw-bold mb-1">Tittle</p>
+                    {products &&
+                        products.map((product, item) => {
+                            return (
+                                <tr key={item}>
+                                    <th scope="row">{item + 1}</th>
+                                    <td><div className="d-flex align-items-center">
+                                        <img
+                                            src={product.image_url}
+                                            alt="IMG"
+                                            style={{ width: "55px", height: "55px" }}
+                                            className="rounded-circle"
+                                        />
+                                        <div className="ms-3">
+                                            <p className="fw-bold mb-1">{product.name}</p>
 
-                            </div>
-                        </div></td>
-                        <td><p className="text-muted mb-0">description</p></td>
-                        <td><span className="badge rounded-pill bg-success">Active</span></td>
-                        <td><span>Quantity</span></td>
-                    </tr>
+                                        </div>
+                                    </div></td>
+                                    <td><p className="text-muted mb-0">{product.description}</p></td>
+                                    <td><span className="badge rounded-pill bg-success">Active</span></td>
+                                    <td><span>{product.quantity}</span></td>
+                                </tr>
+                            )
+                        })
+                    }
 
                 </tbody>
             </table>
