e95f7912d7de372c504279d20b3e7f4f3958e0e1
Frossting <109557017+Frossting@users.noreply.github.com>
2023-10-14 00:36:18 +0000

    qr code added to images

--- src/front/js/component/productCard.js
+++ src/front/js/component/productCard.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/order.css";
+import QRCode from "qrcode";
 
 const ProductCard = ({ onProduct, onQuantity, description, price, image }) => {
+  const [ qrTestProductCode, setQrTestProductCode ] = useState(image) 
+
+  const generateQR = async text => {
+    try {
+      let imageQR = await QRCode.toDataURL(text)
+      setQrTestProductCode(imageQR)
+      return imageQR
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  
+  generateQR(`https://www.youtube.com/watch?v=${description}`)
+  
 
   const productLabel = onProduct[0].toUpperCase() + onProduct.slice(1);
 
   return (
 
     <div className="card d-flex flex-column mt-5 flex-shrink-0" style={{ width: '250px' }}>
-      <img src={image} className="rounded" alt="..." style={{ height: '50%' }} />
+      <img src={qrTestProductCode} className="rounded" alt="..." style={{ height: '50%' }} />
       <div className="card-body">
         <h5 className="card-title">{productLabel}</h5>
         <p className="card-text">{description}</p>
